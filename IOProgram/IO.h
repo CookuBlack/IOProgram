0156108dfbff253909d4460bb3bb4eec4fe013e4
CooKu <125946407+CookuBlack@users.noreply.github.com>
2024-01-03 18:38:55 +0800

    Add files via upload

---
+++ IOProgram/IO.h
@@ -0,0 +1,63 @@
+#include<stdio.h> 
+// 数字定义
+#define I 1
+#define O 0
+// 运算符定义
+#define OIO +
+#define OIOO ++
+#define IOI -
+#define IOII --
+#define OOO =
+#define IOIOI >
+#define OIOIO <
+#define IOIOOI &&
+#define IOIOOII %
+#define IOIIO *
+#define IOIIII /
+#define OIOIIO ||
+// 符号定义
+#define IO (
+#define OO ,
+#define OI )
+#define IOO [
+#define OOI ]  
+#define IOOO {
+#define OOOI }
+#define II ;
+// 关键字定义
+#define IIII main
+#define IIO int
+#define IIOO long
+#define OOII char
+#define IOIO return
+#define OIII printf
+#define IIIO scanf
+#define IOOII for
+#define IOOI while
+#define IOIOII if
+#define IOIOO else
+#define IOIOOIO void
+// 变量名定义
+#define OIIOI array
+#define OIIOIOI l
+#define OIIOIII r
+#define OIIOIOO left
+#define OIIOIIOI right
+#define OIIOIIOIOI pivote
+#define OIIOIIOIOO sizeof 
+#define OIIOIIOIOII mid 
+#define OIIOIIOIO len  
+#define OIIOOIII QuickSort 
+#define OIIOOIIOI Dichotomy
+#define OIIOOIIOII Traverse
+#define OIIOII i
+#define OIIOIIO j
+#define OIIOIIOO temp
+// 字符串定义
+#define OIIIO "%3d "
+#define OIIOOIIO "\n"
+#define OIIOOIIOIO "\t"
+#define OIIIOOI "《冒泡排序》"
+#define OIIIOOIO "《快速排序》"
+#define OIIIOO "排序前的序列为："
+#define OIIIOOO "排序后的序列为："
